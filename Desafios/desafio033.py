dd2518093a61a2ce210e9e69f22e52a46e197d99
fndalemao <fnd.alemao02@gmail.com>
2018-11-08 14:01:26 -0200

    Correção do desafio033

--- Desafios/desafio033.py
+++ Desafios/desafio033.py
@@ -1,11 +1,15 @@
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 n3 = int(input('Digite o terceiro número: '))
-if (n1 > n2, n3):
-    print(n1)
-if (n2 > n1, n3):
-    print(n2)
-if (n3 > n1, n2):
-    print(n3)
-else:
-    print('nada')
+maior = n1
+menor = n3
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+if n1 < n2 and n1 < n3:
+    menor = n2
+if n2 < n1 and n2 < n3:
+    menor = n2
+print('Maior {}'.format(maior))
+print('Menor {}'.format(menor))
